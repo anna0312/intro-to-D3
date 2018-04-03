266187f07a36e5c1111a185e549fbe595d8c0047
anna0312 <annabremner@gmail.com>
2018-04-03 08:04:48 -0400

    Removed code from donut.js

--- assets/scripts/c3/donut.js
+++ assets/scripts/c3/donut.js
@@ -1,24 +1,3 @@
 'use strict'
 
-const c3 = require('c3')
-
-const chart = c3.generate({
-  data: {
-    columns: [
-      ['Pretty awesome', 30],
-      ['Extremely awesome', 120],
-      ['Someone mentioned donuts?', 80]
-    ],
-    type: 'donut',
-    onclick: function (d, i) {
-      console.log('onclick', d, i)
-    }
-  },
-  donut: {
-    title: 'What do you think?'
-  }
-})
-
-setTimeout(function () {
-  chart.load()
-}, 1500)
+// Lets build a chart!
